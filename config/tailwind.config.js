8856e80e0b8a77f0e5baf8fa0a5f4ed161ec8e84
davenport1 <davenmport@gmail.com>
2024-10-22 21:35:35 -0700

    about page, pictures for main page

--- config/tailwind.config.js
+++ config/tailwind.config.js
@@ -18,5 +18,7 @@ module.exports = {
     require('@tailwindcss/forms'),
     require('@tailwindcss/typography'),
     require('@tailwindcss/container-queries'),
+    require('tailwindcss'),
+    require('autoprefixer'),
   ]
 }
