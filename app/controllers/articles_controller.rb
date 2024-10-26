e1a6c502686d15257cd6376971ee2a799425bc08
davenport1 <davenmport@gmail.com>
2024-10-25 18:50:51 -0700

    fixed articles controller

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,5 +1,5 @@
 class ArticlesController < ApplicationController
-  allow_unauthenticated_access only: [ index, show ]
+  allow_unauthenticated_access only: [ :index, :show ]
   # http_basic_authenticate_with name: "dhh", password: "secret", except: [:index, :show]
   def index
     @articles = Article.all
@@ -14,7 +14,7 @@ class ArticlesController < ApplicationController
   end
 
   def create
-    @article = Article.new(title: "...", body: "...")
+    @article = Article.new(article_params)
 
     if @article.save
       redirect_to @article
