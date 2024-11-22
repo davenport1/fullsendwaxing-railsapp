51c3faa411c388bdfe704e3b730a55dbda79906c
davenport1 <davenmport@gmail.com>
2024-11-21 17:53:22 -0800

    add admin helper to authentication.rb concern

--- app/controllers/admin_controller.rb
+++ app/controllers/admin_controller.rb
@@ -3,10 +3,4 @@ class AdminController < ApplicationController
 
   def home
   end
-
-  def require_admin
-    unless current_user&.admin?
-      redirect_to root_path, alert: "You are not authorized to access this page"
-    end
-  end
 end
